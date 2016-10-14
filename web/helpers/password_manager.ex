6f8ee32716ae4eb46c3c689ccb5d5e67bad28195
Elijah Kim <kimelijah90@gmail.com>
2016-10-14 10:25:59 -0400

    Use correct layouts for sign-in/up

--- web/helpers/password_manager.ex
+++ web/helpers/password_manager.ex
@@ -2,9 +2,13 @@ defmodule Looseleaf.PasswordManager do
   alias Looseleaf.{User}
 
   def check(%{"email" => email, "password" => password}, repo) do
-    user = repo.get_by(User, email: email)
+    repo.get_by(User, email: email)
+    |> check_password(password)
+  end
 
-    if Comeonin.Bcrypt.checkpw(password, user.encrypted_password) do
+  defp check_password(nil, _password_guess), do: {:error, :wrong_combination}
+  defp check_password(%{encrypted_password: encrypted_password} = user, password_guess) do
+    if Comeonin.Bcrypt.checkpw(password_guess, encrypted_password) do
       {:ok, user}
     else
       {:error, :wrong_combination}
