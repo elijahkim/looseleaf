af78e370a0ea964a0f0f5682063bfaf60f2dec11
Elijah Kim <kimelijah90@gmail.com>
2016-10-16 18:35:41 -0400

    Refactor chart to use x number of entries
    * Allow analysis to be seen with 3 entries

--- web/static/js/chart/index.js
+++ web/static/js/chart/index.js
@@ -1,6 +1,7 @@
 import Chart from "chart.js";
-import map from "lodash/map";
 import StackBlur from "stackblur-canvas";
+import each from "lodash/each"
+import reverse from "lodash/reverse"
 
 let emotionColors = {
   "anger": "rgba(255, 68, 90, 0.8)", //red
@@ -8,9 +9,11 @@ let emotionColors = {
   "disgust": "rgba(152, 215, 103, 0.9)", //green
   "sadness": "rgba(68, 150, 203, 0.8)", //blue
   "joy": "rgba(255, 223, 43, 0.8)", //yellow
-}
+};
 
-var prevPoints = { 0: 0, 1: 0, 2: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 }
+let emotionsWithOrder = ["joy", "anger", "fear", "sadness", "disgust"];
+
+var prevPoints = {};
 var yIncrement;
 var canvasWidth;
 var canvasHeight;
@@ -18,57 +21,46 @@ var canvasHeight;
 function buildChartForElement(canvas, parent) {
   let entriesElem = document.getElementsByClassName("js-entries")[0];
   let entries = JSON.parse(entriesElem.dataset.entries);
+  let numPoints = entries["anger"].length
+
+  //set default prevPoints to 0
+  each([...Array(numPoints).keys()], (val) => {
+    prevPoints[val] = 0
+  })
 
   function resizeCanvas() {
     canvasWidth = canvas.width = parent.offsetWidth;
     canvasHeight = canvas.height = parent.offsetHeight;
-    yIncrement = canvas.height / 9;
+    yIncrement = canvas.height / (numPoints - 1);
   }
 
   resizeCanvas();
 
   let context = canvas.getContext('2d')
   context.moveTo(0, 0);
-  createShape(context, {data: entries["joy"], color: emotionColors["joy"]});
-  createShape(context, {data: entries["anger"], color: emotionColors["anger"]});
-  createShape(context, {data: entries["fear"], color: emotionColors["fear"]});
-  createShape(context, {data: entries["sadness"], color: emotionColors["sadness"]});
-  createShape(context, {data: entries["disgust"], color: emotionColors["disgust"]});
+
+  each(emotionsWithOrder, (emotion) => {
+    createShape(context, {data: entries[emotion], color: emotionColors[emotion]});
+  })
 
   StackBlur.canvasRGBA(canvas, 0, 0, canvasWidth, canvasHeight, 100)
 }
 
 function createShape(context, params) {
+  let length = params.data.length
   context.lineWidth = 0;
   context.fillStyle = params.color;
   context.strokeStyle = params.color;
   context.beginPath();
   context.moveTo(prevPoints[0], yIncrement * 0);
 
-  drawBackPath(context, params.data, 0);
-  drawBackPath(context, params.data, 1);
-  drawBackPath(context, params.data, 2);
-  drawBackPath(context, params.data, 3);
-  drawBackPath(context, params.data, 4);
-  drawBackPath(context, params.data, 5);
-  drawBackPath(context, params.data, 6);
-  drawBackPath(context, params.data, 7);
-  drawBackPath(context, params.data, 8);
-  drawBackPath(context, params.data, 9);
+  each([...Array(length).keys()], (val) => {
+    drawBackPath(context, params.data, val);
+  })
 
-  // context.lineTo(canvasWidth * params.data[9], yIncrement * 9);
-  // context.stroke();
-
-  drawFrontPath(context, params.data, 9);
-  drawFrontPath(context, params.data, 8);
-  drawFrontPath(context, params.data, 7);
-  drawFrontPath(context, params.data, 6);
-  drawFrontPath(context, params.data, 5);
-  drawFrontPath(context, params.data, 4);
-  drawFrontPath(context, params.data, 3);
-  drawFrontPath(context, params.data, 2);
-  drawFrontPath(context, params.data, 1);
-  drawFrontPath(context, params.data, 0);
+  each(reverse([...Array(length).keys()]), (val) => {
+    drawFrontPath(context, params.data, val);
+  })
 
   context.fill();
 }
