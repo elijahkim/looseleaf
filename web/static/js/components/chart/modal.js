3b4a89834618cc1d126c54177f7e277bbd338831
Elijah Kim <kimelijah90@gmail.com>
2016-10-18 15:35:09 -0400

    add a few missing styles

--- web/static/js/components/chart/modal.js
+++ web/static/js/components/chart/modal.js
@@ -15,27 +15,32 @@ class Modal extends Component {
         <h1 className="analysis__modal-header">Data Point</h1>
         <div className="analysis__modal-datapoints-container">
           <div className="analysis__modal-datapoint-container">
-            <p>{entry.joy}%</p>
+            <p className="analysis__modal-percents">{entry.joy}%</p>
             <p>Joy</p>
           </div>
           <div className="analysis__modal-entrypoint-container">
-            <p>{entry.anger}%</p>
+            <p className="analysis__modal-percents">{entry.anger}%</p>
             <p>Anger</p>
           </div>
           <div className="analysis__modal-entrypoint-container">
-            <p>{entry.fear}%</p>
+            <p className="analysis__modal-percents">{entry.fear}%</p>
             <p>Fear</p>
           </div>
           <div className="analysis__modal-entrypoint-container">
-            <p>{entry.sadness}%</p>
+            <p className="analysis__modal-percents">{entry.sadness}%</p>
             <p>Sadness</p>
           </div>
           <div className="analysis__modal-entrypoint-container">
-            <p>{entry.disgust}%</p>
+            <p className="analysis__modal-percents">{entry.disgust}%</p>
             <p>Disgust</p>
           </div>
         </div>
-        <a onClick={(e) => this.handleClick(e)}>x</a>
+        <a
+          onClick={(e) => this.handleClick(e)}
+          className="analysis__modal-close-button"
+        >
+          x
+        </a>
       </div>
     )
   }
