816fc0d245eab18f7db07e6af71a9270fe0839dc
Elijah Kim <kimelijah90@gmail.com>
2016-10-19 18:02:47 -0400

    play sound

--- web/static/js/components/notes/index.js
+++ web/static/js/components/notes/index.js
@@ -3,12 +3,41 @@ import Tone from "tone";
 
 class Notes extends Component {
   componentDidMount () {
-    var synth = new Tone.Synth().toMaster();
-    synth.triggerAttackRelease("C4", "8n");
+    Tone.Transport.bpm.value = 180;
+
+    var reverb = new Tone.JCReverb(0.4).connect(Tone.Master);
+    var delay = new Tone.FeedbackDelay({ delayTime: '4t', feedback: 0.5, wet: 0.5 })
+    this.synth1 = new Tone.DuoSynth().chain(delay, reverb);
+    var synth2 = new Tone.DuoSynth().chain(delay, reverb);
+    var synth3 = new Tone.DuoSynth().chain(delay, reverb);
+
+    this.synth1.volume.value = -30;
+    synth2.volume.value = -30;
+    synth3.volume.value = -30;
+
+    var loop = new Tone.Loop((time) => {
+      this.synth1.triggerAttackRelease("A4","4n");
+      synth2.triggerAttackRelease("C4","4n");
+      synth3.triggerAttackRelease("E4","4n");
+    }, "1n");
+
+    loop.start("1m");
+    Tone.Transport.start();
+  }
+
+  handleClick(e) {
+    e.preventDefault
+    this.synth1.triggerAttackRelease("A5", "8n");
   }
 
   render() {
-    return <h1>Hello world</h1>
+    return (
+      <div>
+        <a onClick={(e) => this.handleClick(e)}>
+          <h1>Click Me</h1>
+        </a>
+      </div>
+    )
   }
 }
 
