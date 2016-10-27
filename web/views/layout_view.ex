147473b1eb24a3b9fe833f4beb3c3d385c02e573
Elijah Kim <kimelijah90@gmail.com>
2016-10-27 14:16:32 -0400

    nav touch ups

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,23 @@
 defmodule Looseleaf.LayoutView do
   use Looseleaf.Web, :view
+
+  def active_link_class(conn, view) do
+    phoenix_view = conn.private.phoenix_view
+
+    if phoenix_view == view do
+      "layout__nav-button--active"
+    else
+      "layout__nav-button"
+    end
+  end
+
+  def nav_active_link_class(conn, view) do
+    phoenix_view = conn.private.phoenix_view
+
+    if phoenix_view == view do
+      "layout__off-canvas-link--active"
+    else
+      "layout__off-canvas-link"
+    end
+  end
 end
