6ed0e2770205eb7c03496d23ab66170cca644ae3
Elijah Kim <kimelijah90@gmail.com>
2016-10-28 14:34:20 -0400

    Finish About us page
    * Add photos
    * Add social

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,16 @@
 defmodule Looseleaf.PageView do
   use Looseleaf.Web, :view
+
+  def social_link_to(conn, social, link) do
+    link(to: link, class: "about__person-social-link") do
+      tag(:img, src: static_path(conn, get_asset(social)), class:
+      "about__person-social-link-icon")
+    end
+  end
+
+  defp get_asset(:github), do: "/images/github_icon.svg"
+  defp get_asset(:twitter), do: "/images/twitter_icon.svg"
+  defp get_asset(:email), do: "/images/email_icon.svg"
+  defp get_asset(:linked_in), do: "/images/linkedin_icon.svg"
+  defp get_asset(:instagram), do: "/images/instagram_icon.svg"
 end
