7b315f1918e730a9cae680fd4f8eea222c8da055
Elijah Kim <kimelijah90@gmail.com>
2016-11-08 15:14:38 -0500

    WIP - testing auth controller

--- test/controllers/password_controller_test.exs
+++ test/controllers/password_controller_test.exs
@@ -4,8 +4,19 @@ defmodule Looseleaf.PasswordControllerTest do
 
   test "GET /password/:user_id/edit", %{conn: conn} do
     user = insert(:user)
-    conn = get conn, "/password/#{user.id}/edit"
+    conn =
+      guardian_login(user)
+      |> get(conn, "/password/#{user.id}/edit")
 
     assert html_response(conn, 200)
+  end
+
+  def guardian_login(user, token \\ :token, opts \\ []) do
+    build_conn()
+    |> bypass_through(Looseleaf.Router, [:browser, :browser_session, :browser_authenticated])
+    |> get("/")
+    |> Guardian.Plug.sign_in(user, token, opts)
+    |> send_resp(200, "Flush the session yo")
+    |> recycle()
   end
 end
