935dd2b489546dd0d949630357323ed8658b5e86
Elijah Kim <kimelijah90@gmail.com>
2016-11-09 15:01:38 -0500

    strip out controller test

--- test/controllers/password_controller_test.exs
+++ test/controllers/password_controller_test.exs
@@ -1,22 +1,4 @@
 defmodule Looseleaf.PasswordControllerTest do
   use Looseleaf.ConnCase
   import Looseleaf.Factory
-
-  test "GET /password/:user_id/edit", %{conn: conn} do
-    user = insert(:user)
-    conn =
-      guardian_login(user)
-      |> get(conn, "/password/#{user.id}/edit")
-
-    assert html_response(conn, 200)
-  end
-
-  def guardian_login(user, token \\ :token, opts \\ []) do
-    build_conn()
-    |> bypass_through(Looseleaf.Router, [:browser, :browser_session, :browser_authenticated])
-    |> get("/")
-    |> Guardian.Plug.sign_in(user, token, opts)
-    |> send_resp(200, "Flush the session yo")
-    |> recycle()
-  end
 end
